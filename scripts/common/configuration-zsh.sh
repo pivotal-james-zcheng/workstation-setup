1725d005f6e5d714383bb22227bf8183fd547d90
Natalie Tay <ntay@pivotal.io>
2018-08-02 15:58:40 +0800

    Remove setting zsh as default shell

--- scripts/common/configuration-zsh.sh
+++ scripts/common/configuration-zsh.sh
@@ -1,8 +1,8 @@
 echo
-echo "Configuring zsh"
+echo "Installing zsh"
 brew install zsh
-sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
+sudo -s 'echo /usr/local/bin/zsh >> /etc/shells'
 
+echo "Installing oh my zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp files/zshrc.ohmyzsh ~/.zshrc
-
