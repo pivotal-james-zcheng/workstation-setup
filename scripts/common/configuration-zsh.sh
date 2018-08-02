bbc101fe6a551213645dcd2fefb75842a5c9eecf
Natalie Tay <ntay@pivotal.io>
2018-08-02 16:40:52 +0800

    Remove adding zsh to /etc/shells as it's there by default

--- scripts/common/configuration-zsh.sh
+++ scripts/common/configuration-zsh.sh
@@ -1,7 +1,6 @@
 echo
 echo "Installing zsh"
 brew install zsh
-sudo -s 'echo /usr/local/bin/zsh >> /etc/shells'
 
 echo "Installing oh my zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
