94816fe856cd17c0302115b3d2df11eeaa737489
Natalie Tay <natalie.tay@gmail.com>
2018-08-02 10:52:31 +0800

    Fix zsh setup

--- scripts/common/configuration-zsh.sh
+++ scripts/common/configuration-zsh.sh
@@ -4,5 +4,5 @@ brew install zsh
 sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp files/zshrc ~/.zshrc
+cp files/zshrc.ohmyzsh ~/.zshrc
 
