19a76160eb0cf5f6e5506018c53edb0a3be5444e
pivotal-james-zcheng <zcheng@pivotal.io>
2020-02-22 23:04:24 +0800

    Add back installing zsh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,9 @@ fi
 
 # Note: Homebrew needs to be set up first
 source ${MY_DIR}/scripts/common/homebrew.sh
+
 source ${MY_DIR}/scripts/common/configuration-bash.sh
+source ${MY_DIR}/scripts/common/configuration-zsh.sh
 
 # Place any applications that require the user to type in their password here
 brew cask install github
